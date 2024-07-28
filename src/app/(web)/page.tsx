b7da8607ecf08dfcf546d1a42cdc0453513508c6
Hermann Rasch <hermann.rasch93@gmail.com>
2024-07-28 22:47:18 +0200

    Add Gallery and Search Functionality to Page
    Implemented a new Gallery component showcasing room images in a grid layout.
    Created a PageSearch component to facilitate room search and filtering.
    Integrated Search component to handle room type and query input, with navigation to filtered results.
    Enhanced global CSS with responsive design support for gallery and search components.

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,15 @@
+import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
+import PageSearch from "@/components/PageSearch/PageSearch";
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
+      <PageSearch/>
+      {/**featured room */}
+      <Gallery />
+      {/**news letter */}
     </>
   );
 };
