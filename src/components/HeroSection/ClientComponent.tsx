0c4360f942d3c75d5c7d142c7d12e6e482402df8
Hermann Rasch <hermann.rasch93@gmail.com>
2024-07-28 22:43:47 +0200

    Typo Correction

--- src/components/HeroSection/ClientComponent.tsx
+++ src/components/HeroSection/ClientComponent.tsx
@@ -14,16 +14,16 @@ const ClientComponent: FC<Props> = (props) => {
         <section className="flex px-4 items-center gap-12 container mx-auto">
             <div className="py-10 h-full">
                 {heading1}
-                <div className="felx justify-between mt-12">
-                    <div className="felx gap-3 flex-col items-center justify-center">
+                <div className="flex justify-between mt-12">
+                    <div className="flex gap-3 flex-col items-center justify-center">
                         <p className="text-xs lg:text-xl text-center">Basic Room</p>
                         <CountUpNumber duration={3000} endValue={100}/>
                     </div>
-                    <div className="felx gap-3 flex-col items-center justify-center">
+                    <div className="flex gap-3 flex-col items-center justify-center">
                         <p className="text-xs lg:text-xl text-center">Luxury Room</p>
                         <CountUpNumber duration={3000} endValue={50}/>
                     </div>
-                    <div className="felx gap-3 flex-col items-center justify-center">
+                    <div className="flex gap-3 flex-col items-center justify-center">
                         <p className="text-xs lg:text-xl text-center">Suite</p>
                         <CountUpNumber duration={3000} endValue={80}/>
                     </div>
